d38de59009091b0981a609d6d024332dcba40509
Albert Margaryan <66197687+AlbertMargaryan@users.noreply.github.com>
2021-10-06 22:56:09 +0400

    Added combinations

--- index.js
+++ index.js
@@ -10,77 +10,77 @@ const items = [
 		conf: "NaN",
 		cons: "NaN",
 		img: "https://cdn-icons-png.flaticon.com/128/3105/3105807.png",
-		id: 1
+		id: 0
 	},
 	{
 		name: "fire",
 		conf: "NaN",
 		cons: "NaN",
 		img: "https://cdn-icons-png.flaticon.com/128/426/426833.png",
-		id: 2
+		id: 1
 	},
 	{
 		name: "air",
 		conf: "NaN",
 		cons: "NaN",
 		img: "https://cdn-icons-png.flaticon.com/128/2756/2756521.png",
-		id: 3
+		id: 2
 	},
 	{
 		name: "earth",
 		conf: "NaN",
 		cons: "NaN",
 		img: "https://cdn-icons-png.flaticon.com/128/4284/4284458.png",
-		id: 4
+		id: 3
 	},
 	{
 		name: "lake",
 		conf: "water",
 		cons: "water",
 		img: "https://cdn-icons-png.flaticon.com/128/4251/4251896.png",
-		id: 5
+		id: 4
 	},
 	{
 		name: "steam",
 		conf: "water",
 		cons: "fire",
 		img: "https://cdn-icons-png.flaticon.com/128/2451/2451374.png",
-		id: 6
+		id: 5
 	},
 	{
 		name: "dirt",
 		conf: "water",
 		cons: "earth",
 		img: "https://cdn-icons-png.flaticon.com/128/1191/1191622.png",
-		id: 7
+		id: 6
 	},
 	{
 		name: "mountain",
 		conf: "earth",
 		cons: "earth",
 		img: "https://cdn-icons-png.flaticon.com/128/619/619010.png",
-		id: 8
+		id: 7
 	},
 	{
 		name: "lava",
 		conf: "earth",
 		cons: "fire",
 		img: "https://cdn-icons-png.flaticon.com/128/4321/4321227.png",
-		id: 9
+		id: 8
 	},
 	{
 		name: "wind",
 		conf: "air",
 		cons: "air",
 		img: "https://cdn-icons-png.flaticon.com/128/1458/1458984.png",
-		id: 10
+		id: 9
 	},
 	{
 		name: "smoke",
 		conf: "air",
 		cons: "fire",
 		img: "https://cdn-icons-png.flaticon.com/128/3501/3501787.png",
-		id: 11
+		id: 10
 	}
 ]
 
@@ -125,9 +125,9 @@ function dragFirst() {
 		cardImg.classList.add("image_item")
 		cardText.classList.add("text_item")
 
-		cardImg.setAttribute("src", items[newElemId-1].img);
-		majorElement.setAttribute("id", items[newElemId-1].id);
-		cardText.innerText = items[newElemId-1].name;
+		cardImg.setAttribute("src", items[newElemId].img);
+		majorElement.setAttribute("id", items[newElemId].id);
+		cardText.innerText = items[newElemId].name;
 
 		majorElement.append(cardImg);
 		majorElement.append(cardText);
@@ -161,7 +161,8 @@ function dragFirst() {
      		Array.from(secondCond).forEach(function(elem) {
 	  		if(getCoords(element).top < getCoords(elem).top + elem.offsetHeight && getCoords(element).top + element.offsetHeight > getCoords(elem).top && elem !== element) {
 	  			if(getCoords(element).left < getCoords(elem).left + elem.offsetWidth && getCoords(element).left + element.offsetWidth > getCoords(elem).left) {
-	  				console.log("Workin!")
+	  				checkCollab(element.id, elem.id, element, elem)
+
 	  		}
   		}
   	})
@@ -207,7 +208,7 @@ function initChange() {
   	Array.from(secondCond).forEach(function(elem) {
   		if(getCoords(element).top < getCoords(elem).top + elem.offsetHeight && getCoords(element).top + element.offsetHeight > getCoords(elem).top && elem !== element) {
   			if(getCoords(element).left < getCoords(elem).left + elem.offsetWidth && getCoords(element).left + element.offsetWidth > getCoords(elem).left) {
-  				console.log("Workin!")
+  				checkCollab(element.id, elem.id, element, elem)
   			}
   		}
   	})
@@ -235,3 +236,65 @@ function getCoords(elem) {   // кроме IE8-
     left: box.left + pageXOffset
   };
 }
+
+function checkCollab(idF, idS, element, elem) {
+	let colf = items[idF].name;
+	let cols = items[idS].name;
+
+	Array.from(items).forEach(function(e) {
+		if(e.conf === colf && e.cons === cols ) {
+			collab(element, elem, e)
+		}
+		else if(e.conf === cols && e.cons === colf) {
+			collab(element, elem, e)
+		}
+	});
+}
+
+function collab(element, elem, e) {
+
+	document.onmousemove = null;
+
+	let top = (element.getBoundingClientRect().top + elem.getBoundingClientRect().top) / 2;
+	let left = (element.getBoundingClientRect().left + elem.getBoundingClientRect().left) / 2;
+
+	elem.style.top = element.style.top;
+
+	elem.classList.add("animate__animated")
+	element.classList.add("animate__animated")
+	elem.classList.add("animate__fadeOutLeft")
+	element.classList.add("animate__fadeOutRight")
+	setTimeout(() => {elem.remove; element.remove}, 1000)
+
+
+	let newElemId = e.id;
+	let majorContainer = document.querySelector(".second_container")
+	let minorElement = document.createElement("div")
+	let cardImg = document.createElement("img");
+	let cardText = document.createElement("p");
+
+	cardImg.classList.add("image_item")
+	cardText.classList.add("text_item")
+
+	cardImg.setAttribute("src", e.img);
+	minorElement.setAttribute("id", e.id);
+	cardText.innerText = e.name;
+
+	minorElement.append(cardImg);
+	minorElement.append(cardText);
+
+	majorContainer.append(minorElement)
+
+	minorElement.classList.add("secondCond")
+	minorElement.classList.add("animate__animated")
+	minorElement.classList.add("animate__fadeIn")
+
+	setTimeout(() => {minorElement.classList.remove("animate__animated"); minorElement.classList.remove("animate__fadeIn")}, 1000)
+
+	minorElement.style.top = top;
+	minorElement.style.left = left;
+
+	secondCond = document.querySelectorAll('.secondCond');
+	initChange();
+}
+
